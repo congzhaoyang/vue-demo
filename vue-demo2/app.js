427e3fd4e66454145dabd490c4fa6ef3b85f7cfd
congzhaoyang <congzhaoyang1@163.com>
2017-07-17 22:22:56 +0800

    add vue-demo2

---
+++ vue-demo2/app.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todoList: []
+  },
+    created: function(){
+    // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
+    window.onbeforeunload = ()=>{
+      let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
+      window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
+    }
+
+    let oldDataString = window.localStorage.getItem('myTodos')
+    let oldData = JSON.parse(oldDataString)
+    this.todoList = oldData || []
+  },
+  methods: {
+    addTodo: function(){
+      this.todoList.push({
+        title: this.newTodo,
+        createdAt: new Date(),
+        done: false,
+        date: new Date(),
+      })
+      console.log(this.todoList)
+      this.newTodo = '';
+    },
+    removeTodo: function(todo) {
+      let index = this.todoList.indexOf(todo)
+      this.todoList.splice(index, 1)
+    }
+  }
+})  
